17d910d5db9aa77c050288334a952729cc08eb75
Abhishek Kumar <110493863+abhishekabhi187@users.noreply.github.com>
2023-01-07 13:27:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ food = {x: 6, y: 7};
         }
     }
     // if bump into the wall
-    if(snake[0].x >= 18 || snake[0].x <=0 && snake[0].y >= 18 || snake[0].y <=0){
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
         return true;
     }
 }
